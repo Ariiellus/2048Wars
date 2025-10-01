1fb5087d32a971a32ab0e3f271b77881a5f47beb
Ariiellus <arielcardenas95@hotmail.com>
2025-10-01 13:41:57 -0600

    disable dark mode

--- packages/nextjs/components/SwitchTheme.tsx
+++ packages/nextjs/components/SwitchTheme.tsx
@@ -1,22 +1,10 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useTheme } from "next-themes";
 import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 
 export const SwitchTheme = ({ className }: { className?: string }) => {
-  const { setTheme, resolvedTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
-
-  const isDarkMode = resolvedTheme === "dark";
-
-  const handleToggle = () => {
-    if (isDarkMode) {
-      setTheme("light");
-      return;
-    }
-    setTheme("dark");
-  };
 
   useEffect(() => {
     setMounted(true);
