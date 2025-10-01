2eaf5ddafb525649361e77658ad12b4b08d65fa5
Ariiellus <arielcardenas95@hotmail.com>
2025-10-01 13:43:30 -0600

    disable dark mode

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -1,19 +1,11 @@
 import React from "react";
-import { hardhat } from "viem/chains";
 import { HeartIcon } from "@heroicons/react/24/outline";
-// import { SwitchTheme } from "~~/components/SwitchTheme";
 import { BuidlGuidlLogo } from "~~/components/assets/BuidlGuidlLogo";
-import { useTargetNetwork } from "~~/hooks/scaffold-eth/useTargetNetwork";
-import { useGlobalState } from "~~/services/store/store";
 
 /**
  * Site footer
  */
 export const Footer = () => {
-  const nativeCurrencyPrice = useGlobalState(state => state.nativeCurrency.price);
-  const { targetNetwork } = useTargetNetwork();
-  const isLocalNetwork = targetNetwork.id === hardhat.id;
-
   return (
     <div className="min-h-0 py-5 px-1 mb-11 lg:mb-0">
       <div className="w-full">
