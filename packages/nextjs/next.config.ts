400129d6309a03fa0c6e1202aee6244923effef9
Ariiellus <arielcardenas95@hotmail.com>
2025-10-03 11:05:45 -0600

    account association

--- packages/nextjs/next.config.ts
+++ packages/nextjs/next.config.ts
@@ -14,15 +14,6 @@ const nextConfig: NextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
-  async redirects() {
-    return [
-      {
-        source: "/.well-known/farcaster.json",
-        destination: "https://api.farcaster.xyz/miniapps/hosted-manifest/0199aae3-c072-79f2-0f7b-f1c828a13fcc",
-        permanent: false, // 307 redirect
-      },
-    ];
-  },
 };
 
 const isIpfs = process.env.NEXT_PUBLIC_IPFS_BUILD === "true";
